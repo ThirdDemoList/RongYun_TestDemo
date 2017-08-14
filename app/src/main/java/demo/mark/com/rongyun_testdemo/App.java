13519adc1a9d708c6b5f460d6c02452361c20790
Mark_Chen <2285581945@qq.com>
2017-08-14 13:07:49 +0800

    Mark
    融云集成demo   完成到了融云的初始化

---
+++ app/src/main/java/demo/mark/com/rongyun_testdemo/App.java
@@ -0,0 +1,20 @@
+package demo.mark.com.rongyun_testdemo;
+
+import android.app.Application;
+
+import io.rong.imkit.RongIM;
+
+/**
+ * 作者：mark on 2017/8/14 11:56
+ * <p>应用管理主类
+ * 邮箱：2285581945@qq.com
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化融云
+        RongIM.init(this);
+    }
+}
